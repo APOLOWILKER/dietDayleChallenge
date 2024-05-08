4932e3766a31ad97bf1e8b0ded9aab5663f29087
APOLOWILKER <wilkerapolocarvalho@gmail.com>
2024-05-07 23:15:38 -0300

    feat: implemente style fyle

---
+++ src/theme/index.ts
@@ -0,0 +1,39 @@
+export default {
+  COLORS: {
+    PRODUCT: {
+      RED_100: '#F4E6E7',
+      RED_300: '#F3BABD',
+      RED_700: '#BF3B44',
+
+
+      GREEN_100: '#E5F0DB',
+      GREEN_300: '#CBE4B4',
+      GREEN_600: '#639339',
+    },
+    BASE: {
+      WHITE: '#FFFFFF',
+
+
+
+      GRAY_100: '#FAFAFA',
+      GRAY_200: '#EFF0F0',
+      GRAY_300: '#DDDEDF',
+      GRAY_400: '#B9BBBC',
+      GRAY_500: '#5C6265',
+      GRAY_600: '#333638',
+      GRAY_700: '#1B1D1E',
+    }
+  },
+  FONT_FAMILY: {
+    REGULAR: 'Nunito_400Regular',
+    BOLD: 'Nunito_700Bold',
+  },
+  FONT_SIZE: {
+    SM: 12,
+    MD: 14,
+    LG: 16,
+    XL: 18,
+    XXL: 24,
+    XXXL: 32,      
+  },
+}
